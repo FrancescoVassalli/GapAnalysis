3956a3ddcb65fe924ec2dec436e6e80f3dd23f94
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2025-03-13 11:00:36 -0600

    getting bait

--- ui/src/components/bait/BaitEntry.tsx
+++ ui/src/components/bait/BaitEntry.tsx
@@ -8,6 +8,8 @@ interface BaitEntryProps {
   id: number ;
 }
 
+// const queryClient = useQueryClient();
+
 const BaitEntry: FC<BaitEntryProps> = ({ name, id }) => {
   const [enabled, setEnabled] = useState(false);
 
@@ -16,16 +18,21 @@ const BaitEntry: FC<BaitEntryProps> = ({ name, id }) => {
       bait_id: id,
     },
   });
+  console.log(queryOptions);
   useQuery({
     ...queryOptions,
     enabled,
     onSuccess: () => setEnabled(false),
   });
+  function handleShowBaitClick(){
+    setEnabled(true);
+    // queryClient.setQueryData(["showActiveBait"], id);
+  }
   return (
     <div className="p-4 border rounded shadow-sm flex items-center">
       <h2 className="text-xl font-semibold mr-auto">{name}</h2>
       <p>ID: {id} </p>
-      <button onClick={() => setEnabled(true)} className="ml-auto">
+      <button onClick={handleShowBaitClick} className="ml-auto">
         <GhostMail className="w-5 h-5 text-light-haze-900 dark:text-light-haze-100" />
       </button>
     </div>
--- ui/src/routes/bait.tsx
+++ ui/src/routes/bait.tsx
@@ -1,18 +1,38 @@
 import BaitOverview from '@components/bait/BaitOverview';
 import CreateBait from '@components/bait/CreateBait';
+import { useQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 export const Route = createFileRoute('/bait')({
   component: RouteComponent,
 });
 
+// const queryClient = useQueryClient();
+// queryClient.setQueryData(["showActiveBait"], false);
+
+const fetchShowActiveBait = async () => {
+  return false;
+};
+
 function RouteComponent() {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold text-center mb-6">Bait Summary</h1>
-      <div className="space-y-4">
-        <CreateBait />
-        <BaitOverview/>
+  const { data: showActiveBait, isLoading, isError } = useQuery({
+    queryKey: ['showActiveBait'],
+    queryFn: fetchShowActiveBait
+  });
+  if (isLoading===true || isError===true || showActiveBait===false){
+    return (
+      <div className="container mx-auto p-4">
+        <h1 className="text-4xl font-bold text-center mb-6">Bait Summary</h1>
+        <div className="space-y-4">
+          <CreateBait />
+          <BaitOverview/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  else{
+    console.log(`active bait id ${showActiveBait}`);
+    return (
+      <div className="content" dangerouslySetInnerHTML={{__html: "Hello"}}></div>
+    );
+  }
 }
